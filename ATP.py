4c531422407ddeea8e50a77fdaa684a9f812ba3b
Daniel Magalhães <magalhaes.ds@gmail.com>
2020-03-27 14:03:39 -0300

    Novo exercicio

--- ATP.py
+++ ATP.py
@@ -1,15 +1,13 @@
 # Etapa 1
-
-print ('Olá, esta é a CredFácil! Seja bem-vindo! Aqui quem fala é Daniel Sampaio Magalhães')
-print ('Vamos precisar de alguns dados seu para realizar sua análise de crédito, tudo bem?')
+print ('\n\nOlá, esta é a CredFácil! Seja bem-vindo! Aqui quem fala é Daniel Sampaio Magalhães')
+print ('Vamos precisar de alguns dados seu para realizar sua análise de crédito, tudo bem?\n')
 
 cargo = input ('Para comerçarmos, qual seu cargo na empresa em que trabalha atualmente? ')
 salario = input ('Agora, qual seu salário atual? ')
 ano = input ('E por último, qual o ano em que você nasceu? ')
 
-print ('Muito bem!')
-print ('Vamos ver se está tudo certo, ok?')
+print ('\nÓtimo! Vamos ver se está tudo certo, ok?')
 
-print ('Seu cargo atual é: ',cargo, ', Seu salário é de:' ,salario, 'e você nasceu em' ,ano,'.')
+print ('\nSeu cargo atual é: ' + cargo + ', seu salário é de ' + salario + ' e você nasceu em ' + ano + '.')
 
-print ('Muito obrigado pelas informações, até mais!')
+print ('\nMuito obrigado pelas informações, até mais!\n\n')
---
+++ desafio-dec-hex-bi.py
@@ -0,0 +1,9 @@
+
+print ('\n\nConversor para Decimal, Hexadecimal e Binário')
+print ()
+letra = input ('Digite uma letra: ')
+print ('Letra digitada foi: ', letra)
+print ('Seu valor em Decimal é: ', ord(letra))
+print ('Seu valor em Hexadecimal é: ', hex(ord(letra)))
+print ('Seu valor em Binário é: ', bin(ord(letra)))
+print ()
